fcf52fb262686370815aead44a33f5af9aac1f2e
Stefano Pittalis <stefano.pittalis@hotmail.it>
2018-07-22 11:46:41 +0200

    BUG: primo tentativo

--- header/type_useful.h
+++ header/type_useful.h
@@ -5,20 +5,20 @@
 #include <cutil_math.h>
 
 // Defines for constant values
-#define WIDTH 512.0
-#define HEIGHT 512.0
-#define MAX_STEPS 128
-#define EPSILON 0.01f
-#define BLOCK_DIM_X 8
-#define BLOCK_DIM_Y 8
-#define NUM_STREAMS 1
-#define PIXEL_PER_STREAM_X (int)(WIDTH)
-#define PIXEL_PER_STREAM_Y (int)(HEIGHT)
-#define PIXEL_PER_STREAM (int)((WIDTH)*(HEIGHT))
-#define MASK_SIZE 7
-#define FAR_PLANE 100.0f
-#define MAX_NUMBER_OF_FRAMES 1
-#define PARALLEL false
+#define WIDTH					1024.0
+#define HEIGHT					1024.0
+#define MAX_STEPS				128
+#define EPSILON					0.01f
+#define BLOCK_DIM_X				8
+#define BLOCK_DIM_Y				8
+#define NUM_STREAMS				4
+#define PIXEL_PER_STREAM_X		(int)(WIDTH/2)
+#define PIXEL_PER_STREAM_Y		(int)(HEIGHT/2)
+#define PIXEL_PER_STREAM		(int)((WIDTH/2)*(HEIGHT/2))
+#define MASK_SIZE				7
+#define FAR_PLANE				100.0f
+#define MAX_NUMBER_OF_FRAMES	1
+#define PARALLEL				true
 
 // Color of a pixel
 struct pixel
